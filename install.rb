1d0725af0daec2d9d4076d121cac2a752720f277
Eric Entin <eric@ericentin.com>
2009-06-11 10:58:28 -0400

    Install fixed.

--- install.rb
+++ install.rb
@@ -1,5 +1,3 @@
-require 'rails_cache_wurfl'
-
 # Create necessary directory for wurfl.xml/pstore if it doesn't already exist.
 puts 'Creating wurfl directory at' + Rails.root.join('tmp', 'wurfl')
 FileUtils.mkdir_p(Rails.root.join('tmp', 'wurfl'))
