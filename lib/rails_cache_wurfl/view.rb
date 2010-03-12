c73ef12a020f1f1dc3a9c2c894c0e68e1784b339
Armand du Plessis <armanddp@agilisto.com>
2010-03-12 17:19:50 +0200

    Imported some additional helper methods

--- lib/rails_cache_wurfl/view.rb
+++ lib/rails_cache_wurfl/view.rb
@@ -7,17 +7,7 @@ module RailsCacheWurfl
     
     def handset_capability(capability)
       return nil if handset.nil?
-      capability = handset[capability]
-      return nil if capability.nil?
-      case capability.strip
-      when /^d+$/
-        capability = capability.to_i
-      when /^true$/i
-        capability = true
-      when /^false$/i
-        capability = false
-      end
-      return capability
+      handset.capability(capability)
     end
 
   end
---
+++ lib/rails_cache_wurfl/wurfl/wurfl_helpers.rb
@@ -0,0 +1,81 @@
+module WurflHelpers
+  def supports_css?
+    [:low, :mid, :high].include?(xhtml_support_level)
+  end
+  
+  def supported_device?
+    xhtml_support_level != :none
+  end
+  
+  def xhtml_support_level
+    case capability(:xhtml_support_level)
+    when '-1'
+      # no XHTML support at all
+      return :none
+    when '0'
+      # basic XHTML, no CSS support
+      return :basic
+    when '1'
+      # basic XHTML, some CSS support
+      return :low
+    when '2'
+      # much the same as 1, some grey area
+      return :low
+    when '3'
+      # Full XHTML with excellent CSS support
+      return :mid
+    when '4'
+      # Awesomeness, iPhone and so on
+      return :high
+    end
+  end
+  
+  def low?
+    [:none, :basic, :low].include?(xhtml_support_level)
+  end
+  
+  def supports_javascript?
+    capability(:ajax_support_javascript)
+  end
+    
+  def supports_javascript_css_manipulation?
+    capability(:ajax_manipulate_css)
+  end
+  
+  def supports_ajax?
+    capability(:ajax_xhr_type) != 'none'
+  end
+  
+  def supports_j2me_midp_1_0?
+    capability(:j2me_midp_1_0) != 'none'
+  end
+  
+  def supports_j2me_cldc_1_0?
+    capability(:j2me_cldc_1_0) != 'none'
+  end
+  
+  def supports_j2me_midp_2_0?
+    capability(:j2me_midp_2_0) != 'none'
+  end
+  
+  def supports_j2me_cldc_1_1?
+    capability(:j2me_cldc_1_1) != 'none'
+  end
+  
+  def is_wireless_device?
+    capability(:is_wireless_device)
+  end
+  
+  # TODO remove, this is lame
+  def browser_is_wap?
+    capability(:is_wireless_device)
+  end
+
+  def brand
+    capability(:brand_name)
+  end
+
+  def model
+    capability(:model_name)
+  end
+end
