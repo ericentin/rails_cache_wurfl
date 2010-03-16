3a2caa197fc6568ea2e052659750e1250b9312b0
Armand du Plessis <armanddp@agilisto.com>
2010-03-16 12:20:40 +0200

    Simplified controller filter

--- lib/rails_cache_wurfl/filter.rb
+++ lib/rails_cache_wurfl/filter.rb
@@ -1,12 +1,37 @@
 module RailsCacheWurfl
   module Filter
     
-    def set_wurfl
-      return unless session[:handset_checked].nil?
-      handset = RailsCacheWurfl.get_handset(request.headers['HTTP_USER_AGENT'])
-      session[:handset_agent] = handset.user_agent unless handset.nil?
-      session[:handset_checked] = true
+    def self.included(base) 
+      base.extend ActMethods
+    end 
+    
+    module ActMethods 
+      def acts_as_mobile(opts={})
+        $force_mobile = opts.has_key?(:force_mobile) ? opts.delete(:force_mobile) : false
+        unless included_modules.include? InstanceMethods 
+          extend ClassMethods
+          prepend_before_filter :set_mobile_format, :handset
+          helper_method :handset
+          include InstanceMethods
+        end 
+      end 
+    end
+
+    module ClassMethods
     end
     
+    module InstanceMethods            
+      def handset
+        @handset ||= RailsCacheWurfl.get_handset(request.headers['HTTP_USER_AGENT'])
+        # TODO: Revise whether we want to rather cache handset in session. 
+        # Suspect the memcache solution might be quicker than mysql based session. Need to bench
+      end
+      
+      protected
+      def set_mobile_format
+        request.format = :mobile if @handset && @handset.is_wireless_device? && request.format != :js
+        Rails.logger.debug "Request Format #{request.format}"
+      end
+    end
   end
 end
