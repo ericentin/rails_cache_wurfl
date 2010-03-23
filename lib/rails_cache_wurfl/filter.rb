72a2300417ac6613f7d44c25c30230fc75d8f083
Armand du Plessis <armanddp@agilisto.com>
2010-03-24 01:00:53 +0200

    Updated behavior to rather prepend view paths so that layouts, templates and partials can be overridden individually without requiring every view file to be duplicated. Degrades somewhat gracefully now. mobile - high -> mid -> low -> default

--- lib/rails_cache_wurfl/filter.rb
+++ lib/rails_cache_wurfl/filter.rb
@@ -18,19 +18,47 @@ module RailsCacheWurfl
     end
 
     module ClassMethods
+      attr_accessor :xhtml_support_level
     end
     
     module InstanceMethods            
       def handset
+        Rails.logger.debug "LOADING FOR #{request.headers['HTTP_USER_AGENT']}"
         @handset ||= RailsCacheWurfl.get_handset(request.headers['HTTP_USER_AGENT'])
         # TODO: Revise whether we want to rather cache handset in session. 
         # Suspect the memcache solution might be quicker than mysql based session. Need to bench
       end
       
       protected
+        # This no longer sets the request format but instead just give the opportunity to override
+        # layouts, templates or partials for specific device capabilities
       def set_mobile_format
-        request.format = :mobile if @handset && @handset.is_wireless_device? && request.format != :js
-        request.format = :html5 if @handset.user_agent =~ /(iPhone|Android)/
+        if @handset.user_agent =~ /(iPhone|Android)/
+          format, @xhtml_support_level = :html5, @handset.xhtml_support_level
+          prepend_custom_format_view_path(:html5) 
+        elsif @handset && @handset.is_wireless_device? && request.format != :js
+          format, @xhtml_support_level = :mobile,  @handset.xhtml_support_level 
+          prepend_mobile_format_view_path(format, @xhtml_support_level)
+        end
+      end
+      
+      def prepend_mobile_format_view_path(format, level)
+        self.prepend_view_path "app/views_custom/#{format.to_s}"
+        case level
+          when :high 
+            self.prepend_view_path "app/views_custom/#{format.to_s}/low"
+            self.prepend_view_path "app/views_custom/#{format.to_s}/mid"
+            self.prepend_view_path "app/views_custom/#{format.to_s}/high"
+          when :mid
+            self.prepend_view_path "app/views_custom/#{format.to_s}/low"
+            self.prepend_view_path "app/views_custom/#{format.to_s}/mid"
+          when :low
+            self.prepend_view_path "app/views_custom/#{format.to_s}/low"
+        end
+      end
+      
+      def prepend_custom_format_view_path(format)
+        self.prepend_view_path "app/views_custom/#{format.to_s}"
       end
     end
   end
