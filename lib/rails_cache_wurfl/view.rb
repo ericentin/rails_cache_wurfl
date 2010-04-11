fe7a1c437fd450ce730eeb7e0cd0001e0d060e5c
Armand du Plessis <armanddp@agilisto.com>
2010-04-12 00:52:03 +0200

    Added device width helper

--- lib/rails_cache_wurfl/view.rb
+++ lib/rails_cache_wurfl/view.rb
@@ -2,12 +2,20 @@ module RailsCacheWurfl
   module View
     
     def handset
-      RailsCacheWurfl.get_handset(session[:handset_agent])
+      @handset ||= RailsCacheWurfl.get_handset(session[:handset_agent])
     end
     
     def handset_capability(capability)
       return nil if handset.nil?
       handset.capability(capability)
+    end
+    
+    def specify_width_if_mobile
+      if handset.is_wireless_device?
+         " style='width:#{(handset_capability(:resolution_width)) ? handset_capability(:resolution_width) : "240"}px;'"
+       elsif session[:force_agent] == :mobile
+         " style='width:240px;'"
+       end
     end
 
   end
