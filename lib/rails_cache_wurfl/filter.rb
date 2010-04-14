f654fb4f774fe496f8459e2ea410bb02c4bc0b23
Armand du Plessis <armanddp@agilisto.com>
2010-04-14 15:23:31 +0200

    Parked the override changes

--- lib/rails_cache_wurfl/filter.rb
+++ lib/rails_cache_wurfl/filter.rb
@@ -24,19 +24,24 @@ module RailsCacheWurfl
     module InstanceMethods            
       def handset
         @handset ||= RailsCacheWurfl.get_handset(request.headers['HTTP_USER_AGENT'])
-        check_override
-        @handset
         # TODO: Revise whether we want to rather cache handset in session. 
         # Suspect the memcache solution might be quicker than mysql based session. Need to bench
       end
       
       protected
       def check_override
-        @handset.xhtml_support_level = request.params[:ol] if request.params[:ol] #Debugging
+        if request.params[:ol]
+          session[:ol] = request.params[:ol]
+          session[:ol] = request.params[:ol] == "clear" ? nil : request.params[:ol]
+        end
+        if session[:ol]
+          @handset.xhtml_support_level = session[:ol]
+        end
       end
         # This no longer sets the request format but instead just give the opportunity to override
         # layouts, templates or partials for specific device capabilities
       def set_mobile_format
+        #check_override
         if @handset.user_agent =~ /(iPhone|Android)/ || ($force == :html5)
           format, @xhtml_support_level = :html5, @handset.xhtml_support_level
           prepend_mobile_format_view_path(@xhtml_support_level)
@@ -44,6 +49,7 @@ module RailsCacheWurfl
         elsif @handset && @handset.is_wireless_device? && request.format != :js
           format, @xhtml_support_level = :mobile,  @handset.xhtml_support_level 
           prepend_mobile_format_view_path(@xhtml_support_level)
+#          set_content_type
         end
       end
       
@@ -68,6 +74,17 @@ module RailsCacheWurfl
       def prepend_custom_format_view_path(format)
         self.prepend_view_path "app/views_custom/#{format.to_s}"
       end
+      
+      def set_content_type
+       if request.accepts.include?(Mime::Type.new('application/vnd.wap.xhtml+xml'))
+         type = 'application/vnd.wap.xhtml+xml'
+       elsif request.accepts.include?(Mime::Type.new('application/xhtml+xml'))
+         type = 'application/xhtml+xml'
+       else
+         type = 'text/html'
+       end
+       response.content_type = type + "; charset=utf-8;"
+     end
     end
   end
 end
