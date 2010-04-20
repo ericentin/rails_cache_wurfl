9a3c03cb1a3b44190e9b9e3e28acd28902ecfad9
Armand du Plessis <armanddp@agilisto.com>
2010-04-20 02:25:16 +0200

    Removing mobile style rewriting. Instead rely on fallback view paths and templated css.

--- init.rb
+++ init.rb
@@ -2,6 +2,6 @@
 require 'rails_cache_wurfl'
 ActionController::Base.send(:include, RailsCacheWurfl::Filter)
 ActionView::Base.send(:include, RailsCacheWurfl::View)
-ActionView::Base.send(:include, MobileActive::MobileStyleHandler)
-ActionView::Base.send(:alias_method_chain, :stylesheet_link_tag, :mobilization)
+#ActionView::Base.send(:include, MobileActive::MobileStyleHandler)
+#ActionView::Base.send(:alias_method_chain, :stylesheet_link_tag, :mobilization)
 RailsCacheWurfl.init
